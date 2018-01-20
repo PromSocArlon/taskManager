7062760e8437b8733a00aee6bacac931170e7dc4
obawi <albin.lab@gmail.com>
2018-01-20 13:02:44 +0100

    Add save method to WeekDAO

---
+++ includes/classes/WeekDAO.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Albin
+ * Date: 20/01/2018
+ * Time: 11:56
+ */
+
+class TaskDAO
+{
+    /**
+     * Load all the tasks from file to memory.
+     */
+    public function load() {
+
+    }
+
+    /**
+     * Save all the tasks from memory to file.
+     */
+    public function save() {
+
+    }
+}
