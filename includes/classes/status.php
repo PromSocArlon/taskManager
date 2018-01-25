080b10a046eb7a14d33bd8955e209b900a2e69e2
Stephane <vorksprogrammation@gmail.com>
2018-01-25 14:44:05 +0100

    Correction du code

--- includes/classes/status.php
+++ includes/classes/status.php
@@ -8,119 +8,120 @@
 
 require_once 'task.php';
 
-class Status
-{
-    public const A_FAIRE = 1;
-    public const BLOQUE = 2;    // d abord, deja demarree
-    public const EN_COURS = 3;
-    public const INACTIF = 4;   // repoussee p.ex. au "siecle prochain" (comme dans avast, lol)
-    public const TERMINE = 5;
+class Status {
+	public const NON_DEFINI = 0;
+	public const A_FAIRE = 1;
+	public const BLOQUE = 2;    // d abord, deja demarree
+	public const EN_COURS = 3;
+	public const INACTIF = 4;   // repoussee p.ex. au "siecle prochain" (comme dans avast, lol)
+	public const TERMINE = 5;
 
-    protected $statusName;        // < int
-    protected $statusReason;      // < string
-    protected $statusTime;        // < date
+	protected $statusName;        // < int
+	protected $statusReason;      // < string
+	protected $statusTime;        // < date
 
-    private $statusMeaning = array();       // < string[]
+	private $statusMeaning;       // < string[]
 
-    /*public function __construct()
-    {
+	/**
+	 * Status constructor.
+	 * @param int $statusName type of the status
+	 */
+	public function __construct(int $statusName) {
+		$this->defineTheCorrespondencesArray();
+		$this->setstatusReason(time());
+		$this->setStatusName($statusName);
+	}
 
-    }*/
-    public function __construct(int $statusNameInt, string $sName)
-    {
-        if ( DefineTheCorrespondencesArray() )
-        {
-            $this->setstatusReason(time());
-            $this->setStatusName($statusNameInt);
-            $this->setStatusName($sName);
-        }
-        else return null;
-    }
-
-    public function DefineTheCorrespondencesArray() : bool
-    {
-        $this->statusMeaning = [];
-        $this->statusMeaning[] = new Status(Status::A_FAIRE);
-        $this->statusMeaning[] = new Status(Status::BLOQUE);
-        $this->statusMeaning[] = new Status(Status::EN_COURS);
-        $this->statusMeaning[] = new Status(Status::INACTIF);
-        $this->statusMeaning[] = new Status(Status::TERMINE);
-    }
+	private function defineTheCorrespondencesArray(): void {
+		$this->statusMeaning = [];
+		$this->statusMeaning[] = "Non défini";
+		$this->statusMeaning[] = "A faire";
+		$this->statusMeaning[] = "Bloque";
+		$this->statusMeaning[] = "En cours";
+		$this->statusMeaning[] = "Inactif";
+		$this->statusMeaning[] = "Termine";
+	}
 
 
-    /**
-     * Get the last index added to the history of this StatusMeaning(/s) History
-     * @return String Reason
-     */
-    public function getStatusMeaning(int $indexS) : string
-    {
-        if ($indexS>=0 && $indexS<sizeof($this->statusMeaning))
-        {
-            return $this->statusMeaning[$indexS];
-        }
-        return "]Raison_Inexistante ![";
-    }
+	/**
+	 * Get meaning of the current status
+	 * @return String Reason
+	 */
+	public function getStatusMeaning(): string {
+		return $this->statusMeaning[$this->statusName];
+	}
 
-/**************************************************************/
-    /**
-     * Return the status of task in the array
-     * @return int Number of task in the array
-     */
-    public function getStatusName() : int
-    {
-        return $this->statusName;
-    }
-    /**
-     * Set the number of task in the array
-     */
-    public function setStatusName($statusName)             //int
-    {
-        $this->statusName =  $statusName;
-    }
-/**************************************************************/
-    /**************************************************************/
-    /**
-     * Return the reason status of task
-     * @return int Number of task
-     */
-    public function getstatusReason() : string
-    {
-        return $this->statusReason;
-    }
-    /**
-     * Set the number of task
-     */
-    public function setstatusReason($statusReason)          //string
-    {
-        $this->statusReason =  $statusReason;
-    }
-    /**************************************************************/
-    /**************************************************************/
-    /**
-     * Return the time of status
-     * @return date of the status (historically, the last, I suppose)
-     */
-    public function getstatusTime() : date
-    {
-        return $this->statusTime;
-    }
-    /**
-     * Set the number of task in the array
-     */
-    public function setstatusTime($statusTime)             //date
-    {
-        $this->statusTime =  $statusTime;
-    }
-    /**************************************************************/
 
+	/**
+	 * Return the status of task in the array
+	 * @return int Number of task in the array
+	 */
+	public function getStatusName(): int {
+		return $this->statusName;
+	}
+
+
+	/**
+	 * Set the number of task in the array
+	 * @param int $statusName the type of status
+	 */
+	public function setStatusName(int $statusName): void {
+		switch ($statusName) {
+			case Status::A_FAIRE:
+			case Status::BLOQUE:
+			case Status::EN_COURS:
+			case Status::INACTIF:
+			case Status::TERMINE:
+				$this->statusName = $statusName;
+				break;
+			default:
+				$this->statusName = Status::NON_DEFINI;
+		}
+	}
+
+
+	/**
+	 * Return the reason status of task
+	 * @return string The reason of this status
+	 */
+	public function getStatusReason(): string {
+		return $this->statusReason;
+	}
+
+	/**
+	 * Set the number of task
+	 * @param string $statusReason : Can't be an empty string
+	 */
+	public function setStatusReason(string $statusReason): void {
+		if ($statusReason != "") {
+			$this->statusReason = $statusReason;
+		}
+	}
+
+	/**
+	 * Return the time of status
+	 * @return date of the status (historically, the last, I suppose)
+	 */
+	public function getStatusTime(): date {
+		return $this->statusTime;
+	}
+
+	/**
+	 * Set the number of task in the array
+	 * @param date $statusTime : date can't be null
+	 */
+	public function setStatusTime(date $statusTime): void {
+		if ($statusTime) {
+			$this->statusTime = $statusTime;
+		}
+	}
 
 
 	/**
 	 * Return a summary, if asked or needed
 	 * @return string
 	 */
-	public function toString(): string
-    {
+	public function toString(): string {
 	}
 
 }
