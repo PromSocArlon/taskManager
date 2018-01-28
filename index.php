5029709a070887381482285591699f08c1468791
Alain <Alain@DESKTOP-C65UO8O>
2018-01-28 14:39:59 +0100

    Create header/footer to isolate main content

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
-require_once 'includes/functions.php';
-require_once 'includes/configuration.php';
-require_once 'includes/classes/Storage.php';
-require_once 'includes/classes/StorageMysql.php';
-require_once 'includes/classes/task.php';
-require_once 'includes/classes/week.php';
-require_once 'includes/classes/day.php';
+require_once 'application/core/functions.php';
+require_once 'application/models/Storage/configuration.php';
+require_once 'application/models/Storage/Storage.php';
+require_once 'application/models/Storage/StorageMysql.php';
+require_once 'application/models/Entity/task.php';
+require_once 'application/models/Entity/week.php';
+require_once 'application/models/Entity/day.php';
 
+require_once 'application/core/services/controller.php';
+require_once 'application/core/services/dispatcher.php';
+require_once 'application/core/services/request.php';
+require_once 'application/core/services/route.php';
+require_once 'application/core/services/view.php';
+
+$route = new route();
+$route->routeQuery();
+
+/*
 $handle = fopen("php://stdin", "r");
 
 // Select Storage Type
@@ -101,3 +111,4 @@ do {
     }
 
 } while (strtolower($action) != 's');
+*/
