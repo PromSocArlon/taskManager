48a36f928e0fb7b7e5dc3886a2e0696d4bb8cde4
ChrisGligor <chris_daboss@yahoo.com>
2018-01-29 12:15:52 +0100

    Updated taskController

--- application/controllers/taskController.php
+++ application/controllers/taskController.php
@@ -5,12 +5,39 @@ namespace TaskManager\controller;
 
 class taskController
 {
-    public function save($taskName, $taskPriority) {
+    private $model;
+
+    //$model needs to be a Task class type
+    public function __construct($model){
+        $this->model = $model;
+    }
+
+    public function create($taskName, $taskPriority, $taskDescription, $taskStatus){
+
+        $this->model->setName($taskName);
+        $this->model->setPriority($taskPriority);
+        $this->model->setDescription($taskDescription);
+        $this->model->addStatus($taskStatus, $taskDescription);
+    }
+
+    public function update(){
+        // TODO: A completer
+    }
+
+    public function delete(){
+
+        $this->model = 0;
+    }
+
+    public function save() {
 
         $storageFactory = new StorageFactory();
-        $task = new task($storageFactory->getStorage(), $taskName);
-        $task->setName($taskName);
-        $task->setPriority($taskPriority);
-
+        $task = new task($storageFactory->getStorage($this->model->getStorage()));
+        $task->setName($this->model->getName());
+        $task->setPriority($this->model->getPriority());
+        $task->setDescription($this->model->getDescription());
+        $task->addStatus($this->model->getStatus());
     }
+
+
 }
