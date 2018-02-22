19bcb29005ce91f8ae772167c2eb7e306c5de444
Stephane <vorksprogrammation@gmail.com>
2018-02-22 21:29:07 +0100

    Index FileStorage epuration

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-$file = fopen(realpath('application/models/Storage/Storage.php'), "w");
-print_r($file);
-
 require_once 'application/core/functions.php';
 require_once 'application/models/Storage/Storage.php';
 require_once 'application/models/Storage/StorageMysql.php';
