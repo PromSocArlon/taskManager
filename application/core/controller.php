20bd969cd30bfb3f35c26e518999742b3b1bcc80
Stephane <vorksprogrammation@gmail.com>
2018-02-23 03:35:20 +0100

    Controller PHPDOC

--- application/core/controller.php
+++ application/core/controller.php
@@ -3,45 +3,66 @@
 require_once 'request.php';
 require_once 'view.php';
 
-abstract class controller {
+abstract class Controller {
 
     private $action;
     protected $request;
 
-    public function setRequest(request $request)
-    {
-        $this->request = $request;
+    public abstract function index();
+
+    /**
+     * Set the the request attribute to the request parameter value
+     * @param request $request request that called the controller
+     */
+    public function setRequest(request $request) : void {
+        if ($request) {
+            $this->request = $request;
+        }
     }
 
-    public function executeAction($action)
-    {
+    /**
+     * Execute the given action
+     * @param string $action the action to perform
+     * @throws Exception if action not defined
+     */
+    public function executeAction(string $action) : void {
         if (method_exists($this, $action)) {
             $this->action = $action;
             $this->{$this->action}();
         }
         else {
-            $classControleur = get_class($this);
-            throw new Exception("Action '$action' not defined in the class $classControleur");
+            $classController = get_class($this);
+            throw new Exception("Action '$action' not defined in the class $classController");
         }
     }
-    
-    public abstract function index();
-    
-    protected function generateView($data = array())
-    {
 
-        $classControleur = get_class($this);
-        $controller = str_replace("Controller", "", $classControleur);
-        
-        $view= new view($this->action, $controller);
+    /**
+     * Generate the view with a given data set
+     * @param array $data the data set to be added to the generation
+     */
+    protected function generateView(array $data = array()) : void {
+        $classController = get_class($this);
+        $controller = str_replace("Controller", "", $classController);
+        $view = new View($this->action, $controller);
         $view->generate($data);
     }
 
-    public function model($model)
-    {
-        if ($model == 'StorageFactory') require_once 'application/models/Storage/' . $model . '.php';
-        else require_once 'application/models/Entity/' . $model . '.php';
-        return new $model();
+
+    /**
+     * Give an instance of the given class.
+     * @param string $model the wanted model object
+     * @return object the object of the wanted model.
+     * @throws Exception if class not found
+     */
+    public function model(string $model): object {
+        $modelFile = $model == 'StorageFactory' ? 'application/models/Storage/StorageFactory.php' : 'application/models/Entity/' . $model . '.php' ;
+        if (file_exists($modelFile)) {
+            require_once($modelFile);
+            return new $model();
+        } else {
+            throw new Exception("File '".$modelFile."' not found.");
+        }
+
     }
 
 }
