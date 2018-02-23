0c1331f9bca30965234832846be92cdcc4ad7f73
Stephane <vorksprogrammation@gmail.com>
2018-02-23 02:12:12 +0100

    Request PHPDOC

--- application/core/request.php
+++ application/core/request.php
@@ -1,22 +1,31 @@
 <?php
 
 class request {
-
     private $parameters;
 
-    public function __construct($parameters) {
-        $this->parameters= $parameters;
+    public function __construct(array $parameters) {
+        $this->parameters = $parameters;
     }
 
-    public function getParameter($parameterName){
+    /**
+     * @param $parameterName
+     * @return string parameter's value
+     * @throws Exception if parameter doesn't exist
+     */
+    public function getParameter(string $parameterName) : string{
         if ($this->existParameter($parameterName)){
             return $this->parameters[$parameterName];
-        }else{
+        }
+        else {
             throw new Exception("Missing parameter '$parameterName'.");
         }
     }
 
-    public function existParameter($parameterName){
+    /**
+     * @param string $parameterName
+     * @return bool true if this parameter exist and is not null, false otherwise.
+     */
+    public function existParameter(string $parameterName) : bool {
         return (isset($this->parameters[$parameterName]) && $this->parameters[$parameterName] != "");
     }
 }
