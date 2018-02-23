d6ade533985df6e55b2d5487c32855238102778a
Stephane <vorksprogrammation@gmail.com>
2018-02-23 02:13:05 +0100

    Index require Epuration

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php
 
 require_once 'application/core/functions.php';
-require_once 'application/models/Storage/Storage.php';
-require_once 'application/models/Storage/StorageMysql.php';
-require_once 'application/models/Entity/task.php';
-require_once 'application/models/Entity/week.php';
-require_once 'application/models/Entity/day.php';
-
-require_once 'application/core/services/controller.php';
-require_once 'application/core/services/dispatcher.php';
-require_once 'application/core/services/request.php';
-require_once 'application/core/services/route.php';
-require_once 'application/core/services/view.php';
+require_once 'application/core/route.php';
 
 $route = new route();
 $route->routeQuery();
