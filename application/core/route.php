8d6f822f7cd2dd4e48884ac84f42d09e8bb455eb
Stephane <vorksprogrammation@gmail.com>
2018-02-23 02:09:08 +0100

    Route PHPDoc

--- application/core/route.php
+++ application/core/route.php
@@ -1,14 +1,18 @@
 <?php
 
+require_once 'view.php';
+require_once 'request.php';
+
 class route {
 
-    public function routeQuery() {
+    /**
+     *  Analyse the URL and perform the action given by a specific controller
+     */
+    public function routeQuery() : void {
         try {
             $request= new request(array_merge($_GET, $_POST));
-
-            $controller=$this->getNewController($request);
+            $controller = $this->getNewController($request);
             $action = $this->getNewAction($request);
-    
             $controller->executeAction($action);
         }
         catch (Exception $ex) {
@@ -16,38 +20,60 @@ class route {
         }
     }
 
-    private function getNewController(request $request) {
+    /**
+     * Give the controller wanted by a request
+     * @param request $request request that create the controller
+     * @return Controller the wanted controller
+     * @throws Exception if controller doesn't exist
+     */
+    private function getNewController(request $request) : Controller {
         $controllerValue= "home";
-        if ($request->existParameter('controller')){
-            $controllerValue=$request->getParameter('controller');
-            $controllerValue=strtolower($controllerValue);
+        if ($request->existParameter('Controller')) {
+            $controllerValue = $request->getParameter('Controller');
+            $controllerValue = strtolower($controllerValue);
         }
         $classController = $controllerValue . "Controller";
         $fileController= "application/controllers/" . $classController . ".php";
 
-        if (file_exists($fileController)){
+        if (file_exists($fileController)) {
+            /** @var string $fileController */
             require($fileController);
 
             $controller = new $classController();
-             if (isset($request)) {
+             if (isset($request) && $controller instanceof Controller) {
                 $controller->setRequest($request);
              }
-            return $controller;
-        }else {
+             return $controller;
+        }
+        else {
             throw new Exception("File '$fileController' nor found.");
         }
     }
 
-    private function getNewAction(request $request) {
-        $action ="index"; 
-        if ($request->existParameter('action')){
-            $action =$request->getParameter('action');
+    /**
+     * Give the action wanted by a request
+     * @param request $request
+     * @return string the type of action to perform
+     */
+    private function getNewAction(request $request) : string {
+        $action = "index";
+        try {
+            $action = $request->getParameter('action');
         }
-        return $action;
+        catch (Exception $e) {
+            $action ="index";
+        }
+        finally {
+            return $action;
+        }
     }
 
-    private function handleError(Exception $exception){
-        $view= new view('template');
+    /**
+     * Handle any error.
+     * @param Exception $exception the error that must be handled
+     */
+    private function handleError(Exception $exception) : void {
+        $view = new view('template');
         $view->generate(array('msgError'=> $exception->getMessage()));
     }
 
