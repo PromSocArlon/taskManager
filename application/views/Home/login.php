cb7d55c07778459e2268a26235a1fc367cd76e8e
Stephane <vorksprogrammation@gmail.com>
2018-03-04 18:15:14 +0100

    refacto loginForm

---
+++ application/views/Home/login.php
@@ -0,0 +1,49 @@
+<?php
+require_once __DIR__.'/../../core/bootstrapTheme.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <?php echo getBootstrapTag(); ?>
+    <style>
+        .form-border {
+            border: 1px solid #CCCCCC;
+            padding: 10px 30px 10px 30px;
+            border-radius: 10px;
+            margin: 50px auto;
+            box-shadow: 2px 2px 2px 0 #CCCCCC;
+        }
+    </style>
+
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="form-border">
+                <h2>Login Form</h2>
+                <form role="form" action="" method="post">
+                    <div class="form-group">
+                        <label for="loginID"> Username:</label>
+                        <input type="text" class="form-control" name="loginID" placeholder="Enter username"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="loginPassword">Password:</label>
+                        <input type="password" name="loginPassword" class="form-control" placeholder="Enter password"/>
+                    </div>
+                    <div class="checkbox">
+                        <label><input type="checkbox">Remember me</label>
+                    </div>
+                    <input type="submit" class="btn btn-info"/>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
