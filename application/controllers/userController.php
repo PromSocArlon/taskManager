77e7a85d63b0c38bc3aeef10e9b9e796a9545033
Stephane <vorksprogrammation@gmail.com>
2018-03-06 16:16:06 +0100

    LogoutFunction

--- application/controllers/userController.php
+++ application/controllers/userController.php
@@ -7,6 +7,7 @@
  */
 
 require_once __DIR__.'/../models/Entity/Member.php';
+session_start();
 
 class userController extends Controller{
 
@@ -19,15 +20,22 @@ class userController extends Controller{
 
     public function index() {
     	require_once(__DIR__."/../views/_shared/header.php");
-	session_start();
         if(isset($_SESSION['user'])) {
         	$test = $_SESSION['user'];
         	if ($test instanceof Member) {
 			echo $test->getLogin().': '.$test->getPassword();
-	        }
+	                echo '<a href="index.php?controller=user&action=logout">Logout</a>';
+        	}
         }
         //$this->generateView();
         require_once(__DIR__."/../views/_shared/footer.php");
+    }
+
+    public function logout() {
+    	if (isset($_SESSION['user'])) {
+    		unset($_SESSION['user']);
+        }
+        header('Location: index.php/controller=home');
     }
 
     public function save() {
