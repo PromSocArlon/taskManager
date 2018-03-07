374f41b1ae9c22f1a562cd59d9c753d9fab7b827
Stephane <vorksprogrammation@gmail.com>
2018-03-07 16:08:24 +0100

    userService +> refactor getCurrentUser

--- application/core/userService.php
+++ application/core/userService.php
@@ -14,23 +14,25 @@ class UserService
      * @return copie de session['user'] / null si aucune
      * @throws Exception if session['user'] doesn't exist
      */
-    public static function getCurrentUser() : void
+    public static function getCurrentUser() : Member
     {
-        if (isset($_SESSION['name'])) {
-            $cuName = $_SESSION['name'];
-            out($cuName);
-        } else {
-            $cuName = null;
-            out("pas de user setté pour le moment");
-        }
+//        if (isset($_SESSION['name'])) {
+//            $cuName = $_SESSION['name'];
+//            out($cuName);
+//        } else {
+//            $cuName = null;
+//            out("pas de user setté pour le moment");
+//        }
+//
+//        if (isset($_SESSION['password'])) {
+//            $cuPsw = $_SESSION['password'];
+//            out($cuPsw);
+//        } else {
+//            $cuPsw = null;
+//            out("pas de psw setté pour le moment");
+//        }
 
-        if (isset($_SESSION['password'])) {
-            $cuPsw = $_SESSION['password'];
-            out($cuPsw);
-        } else {
-            $cuPsw = null;
-            out("pas de psw setté pour le moment");
-        }
+	    return isset($_SESSION['user']) ? $temp = $_SESSION['user'] : null;
     }
 }
 
