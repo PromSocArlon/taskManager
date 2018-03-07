713935a763bc706271e0eebc929cb064badf4a2c
MaxenceBeernaerts <maxence.beernaerts@hotmail.fr>
2018-03-07 15:05:20 +0100

    View Index, Register (A voir avec Julien)

--- application/views/Home/index.php
+++ application/views/Home/index.php
@@ -1,16 +1,85 @@
-<form action = "http://localhost/Task/?controller=home&action=session" method="post">
-    <fieldset>
-        <legend>Identifiant</legend>
-        <p>
-            <label for="login">Login :</label>
-            <input type="text" name="login" id="login" placeholder="login" />
-        </p>
-        <p>
-            <label for="password">Mot de passe :</label>
-            <input type="password" name="password" id="password" placeholder="password" />
-            <input type="submit" name="submit" value="Identification" />
-            <button><a href="http://localhost/Task/?controller=home&action=register"> Pas encore inscrit ?</a></button>
-        </p>
-    </fieldset>
-</form>
 
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="templateIndex/images/icons/favicon.ico">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateIndex/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateIndex/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateIndex/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateIndex/vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateIndex/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateIndex/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateIndex/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateIndex/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateIndex/css/util.css">
+	<link rel="stylesheet" type="text/css" href="templateIndex/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('templateIndex/images/bg-01.jpg');">
+			<div class="wrap-login100 p-t-30 p-b-50">
+				<span class="login100-form-title p-b-41">
+					Account Login
+				</span>
+				<form class="login100-form validate-form p-b-33 p-t-5" action = "http://localhost/taskManager/?controller=home&action=session" method="post">
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" type="text" name="username" placeholder="User name">
+						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-32">
+						<button class="login100-form-btn">
+							Login
+						</button>
+						
+					</div>
+					
+					<div class="container-login100-form-btn m-t-32">
+						<button class="login100-form-btn">
+							Subscribe
+						</button>
+						
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="templateIndex/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="templateIndex/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="templateIndex/vendor/bootstrap/js/popper.js"></script>
+	<script src="templateIndex/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="templateIndex/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="templateIndex/vendor/daterangepicker/moment.min.js"></script>
+	<script src="templateIndex/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="templateIndex/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="templateIndex/js/main.js"></script>
+
+</body>
+</html>
--- application/views/Home/register.php
+++ application/views/Home/register.php
@@ -1,31 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Register</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="templateRegister/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateRegister/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateRegister/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateRegister/vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateRegister/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateRegister/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateRegister/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateRegister/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="templateRegister/css/util.css">
+	<link rel="stylesheet" type="text/css" href="templateRegister/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
 
-<div class="backgroundColor">
-    <form method="POST" action="">
-        <div class="form-group">
-            <label for="InputEmail1">Email address</label>
-            <input type="email" name = "mail" placeholder="Enter mail">
-            <small >We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-            <label>Team</label>
-            <input type="text" name="team" placeholder="de 1 a 5"  >
-        </div>
-        <div class="form-group">
-            <label for="login">Login :</label>
-            <input type="text" name="login" id="login" placeholder="login"  />
-        </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" name="password"  placeholder="Password">
-            <input type="password" name="password1" placeholder="Confirm Password">
-        </div>
-</div>
-<div class="form-check disabled">
-    <label class="form-check-label">
-        <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios3" value="option3">
-        I agree
-    </label>
-</div>
-<button type="submit"  class="btn btn-primary">register</button>
-</form>
-</div>
+
+	<div class="container-contact100" style="background-color: black;">
+		<div class="wrap-contact100">
+			<form class="contact100-form validate-form">
+				<span class="contact100-form-title">
+					Register :
+				</span>
+
+				<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<span class="label-input100">Email</span>
+					<input class="input100" type="text" name="email" placeholder="Enter your email address">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 input100-select">
+					<span class="label-input100">Team</span>
+					<div>
+						<select class="selection-2" name="service">
+							<option>Team 1</option>
+							<option>Team 2</option>
+							<option>Team 3</option>
+							<option>Team 4</option>
+							<option>Team 5</option>
+						</select>
+					</div>
+					<span class="focus-input100"></span>
+				</div>
+				
+				<div class="wrap-input100 validate-input" >
+					<span class="label-input100">Login</span>
+					<input class="input100" type="text" name="email" placeholder="Enter your login">
+					<span class="focus-input100"></span>
+				</div>
+				
+				<div class="wrap-input100 validate-input" >
+					<span class="label-input100">Password</span>
+					<input class="input100" type="password" name="email" placeholder="Enter your password">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 validate-input" >
+					<span class="label-input100">Password confirmation</span>
+					<input class="input100" type="password" name="email" placeholder="Confirm your password>
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="container-contact100-form-btn">
+					<div class="wrap-contact100-form-btn">
+						<div class="contact100-form-bgbtn"></div>
+						<button class="contact100-form-btn">
+							<span>
+								Submit
+								<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+							</span>
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+
+
+	<div id="dropDownSelect1"></div>
+
+<!--===============================================================================================-->
+	<script src="templateRegister/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="templateRegister/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="templateRegister/vendor/bootstrap/js/popper.js"></script>
+	<script src="templateRegister/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="templateRegister/vendor/select2/select2.min.js"></script>
+	<script>
+		$(".selection-2").select2({
+			minimumResultsForSearch: 20,
+			dropdownParent: $('#dropDownSelect1')
+		});
+	</script>
+<!--===============================================================================================-->
+	<script src="templateRegister/vendor/daterangepicker/moment.min.js"></script>
+	<script src="templateRegister/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="templateRegister/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="templateRegister/js/main.js"></script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+
+</body>
+</html>
