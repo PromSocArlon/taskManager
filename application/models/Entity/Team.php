5a9d8da255dfdf6def3e0ffafcf1d694891731ff
Alain-D <2YuvvM4Xg2Mz1426>
2018-03-11 15:31:15 +0100

    Renamed property , to match logic for Db

--- application/models/Entity/Team.php
+++ application/models/Entity/Team.php
@@ -2,57 +2,58 @@
 require_once('Member.php');
 /* MAJ */
 class Team{
-    private $teamID;
-    private $teamName;
-    private $teamMember = array ();
-    private $teamLeader;
-    private $teamTask = array();
+    private $id;
+    private $name;
+    private $members = array();
+    private $leader;
+    private $tasks = array();
 
 
-    public function __construct($idMember)
+    //public function __construct($idMember)
+    public function __construct()
     {
-        $this->teamMember[0] = new Member($idMember);
+        $this->members[0] = new Member();
     }
 
 
     public function set_tName($new_teamName) {                              //save in database
-		$this->teamName = $new_teamName;
+        $this->name = $new_teamName;
 	}
 	public function get_tName() {                                           // from database
-		return $this->teamName;
+        return $this->name;
 	}	
 	
 	
 	public function set_tID($new_teamID) {                      // $New_teamID from database (sizeof team +1)
-		$this->teamID = $new_teamID;                             //save in database
+        $this->Id = $new_teamID;                             //save in database
 	}
 	 public function get_tID() {                                // from database too
-		return $this->teamID;
+         return $this->id;
 	}	
 	
 	
 	public function set_tMember($new_IdMember) {
         //$new_IdMember  must be tested before , if exists in teamMember throw exception and propose another teamMemeber)
-        array_push($this->teamMember,$new_IdMember);            //save in database
+        array_push($this->members, $new_IdMember);            //save in database
 	}
 
 	public function get_tMember() {
 		//or get from database
-        return $this->teamMember;
+        return $this->members;
 	}	
 	
 	
 	public function set_tLeader($new_teamLeader) {
-		$this->teamLeader = $new_teamLeader;
+        $this->leader = $new_teamLeader;
 	}
 	public function get_tLeader() {
-		return $this->teamLeader;
+        return $this->leader;
 	}
 
 	public function set_tTask($new_teamTask){
-        array_push($this->teamTask, $new_teamTask);           //save in database	}
+        array_push($this->tasks, $new_teamTask);           //save in database	}
     }
         public function get_tTask() {
-		return $this->teamTask; // or get from database
+            return $this->tasks; // or get from database
 	}
 }
