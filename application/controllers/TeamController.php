78c1faf900b81dcdc61c437a07692a6ceebbed28
Alain-D <2YuvvM4Xg2Mz1426>
2018-03-11 15:39:26 +0100

    Edited function save & initializeModel to test TeamDAO

--- application/controllers/TeamController.php
+++ application/controllers/TeamController.php
@@ -2,47 +2,68 @@
 
 //namespace TaskManager\controller;
 
-require_once 'application/core/Controller.php';
+require_once '../core/Controller.php';
 
 class TeamController extends Controller
 {
+    private $team;
+    private $storage;
 
-    public function index() {
+    public function index()
+    {
         $this->generateView();
+    }
+
+    public function save()
+    {
+
+        try {
+            $this->initializeModel();
+            $this->storage = $this->model('teamDAO');
+            $this->storage->create($this->team, 0);
+            $this->redirect('team', 'index');
+        } catch (Exception $ex) {
+            $this->generateView($ex);
+        }
     }
 
     public function initializeModel()
     {
-        // TODO: Implement initialize() method.
+        try {
+            $this->team = $this->model('team');
+            $this->team->set_tName($this->request->getParameter('teamName'));
+        } catch (Exception $ex) {
+            throw;
+        }
     }
 
     /**
      * Show information for one team
      */
-    public function showAction()
-    {
-    }
+    //public function showAction()
+    //{
+    //}
 
-    public function save($teamName, $teamID, $teamMember, $teamLeader, $teamTask)
-    {
-        $storageFactory = new StorageFactory();
-        $team = new team($storageFactory->getStorage());
-        $team->setFirstName($teamName);
-        $team->setLastName($teamID);
-        $team->setTeamMember($teamMember);
-        $team->setTeamLeader($teamLeader);
-        $team->setPassword($teamTask);
-        /*
-        $teamService = new TeamService();
-        $teamService->save($team);
-        */
-        $vue = new UserView();
-        if ($team->save()) {
-            $vue->displayTeam($team);
-        } else {
-            $vue->displayError($team->getErrors());
-        }
-    }
+//    public function save($teamName, $teamID, $teamMember, $teamLeader, $teamTask)
+//    {
+//        $storageFactory = new StorageFactory();
+//        $team = new team($storageFactory->getStorage());
+//        $team->setFirstName($teamName);
+//        $team->setLastName($teamID);
+//        $team->setTeamMember($teamMember);
+//        $team->setTeamLeader($teamLeader);
+//        $team->setPassword($teamTask);
+//        /*
+//        $teamService = new TeamService();
+//        $teamService->save($team);
+//        */
+//        $vue = new UserView();
+//        if ($team->save()) {
+//            $vue->displayTeam($team);
+//        } else {
+//            $vue->displayError($team->getErrors());
+//        }
+//    }
     /**
      * public function listAction(){
      * }
