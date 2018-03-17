0e37a9b436e01a708ab55835a78636358b14e2aa
Alain-D <2YuvvM4Xg2Mz1426>
2018-03-17 11:40:20 +0100

    Create link Team and Member

---
+++ application/models/DAO/MemberTeamDAO.php
@@ -0,0 +1,29 @@
+<?php
+
+class MemberTeamDAO extends DAO
+{
+    protected function objectToArray($arguments)
+    {
+        $array['member_team'] = [];
+
+        if (!empty($arguments)) {
+
+            $memberTeamArray = (array)$arguments[0];
+
+            foreach ($memberTeamArray as $key => $value) {
+                $prop = trim(strtolower($key));
+                switch ($prop) {
+                    case "memberid":
+                        $array['member_team'][0]['new']['fk_member_id'] = $value;
+                        break;
+                    case "teamid":
+                        $array['member_team'][0]['new']['fk_team_id'] = $value;
+                        break;
+                    default:
+                        throw new  Exception("Unhandled property");
+                }
+            }
+        }
+        return $array;
+    }
+}
