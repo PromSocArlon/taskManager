e6ff14bd8e77257d26680259513bc9c47f685678
Alain-D <2YuvvM4Xg2Mz1426>
2018-03-17 14:12:16 +0100

    removed unneeded index.php

--- application/views/Team/index.php
+++ application/views/Team/index.php
@@ -2,16 +2,16 @@
     <br><br><br>
     <p>Welcome to the view Team Index.</p>
 
-    <form method="POST" action="index.php?controller=team&action=create">
+    <form method="POST" action="?controller=team&action=create">
         <input type="submit" name="button" value="Create a team">
     </form>
-    <form method="POST" action="index.php?controller=team&action=read">
+    <form method="POST" action="?controller=team&action=read">
         <input type="submit" name="button" value="Show a team">
     </form>
-    <form method="POST" action="index.php?controller=team&action=update">
+    <form method="POST" action="?controller=team&action=update">
         <input type="submit" name="button" value="Update a team">
     </form>
-    <form method="POST" action="index.php?controller=team&action=delete">
+    <form method="POST" action="?controller=team&action=delete">
         <input type="submit" name="button" value="Delete a team">
     </form>
 </div>
