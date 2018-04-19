35b70e3e6ecbbdc3a5969ca3c92557c2d26f5cb3
Stephane <vorksprogrammation@gmail.com>
2018-04-19 14:01:34 +0200

    Change Preparation

--- application/core/exceptions/ErrorHandler.php
+++ application/core/exceptions/ErrorHandler.php
@@ -13,26 +13,31 @@ use app\controllers\ErrorController;
 
 class ErrorHandler {
     public static function handleError(\Throwable $ex) : void {
-            switch (true) {
-                case $ex instanceof \HttpRequestException : $code = 400;
-                    break;
-                case $ex instanceof UnauthorizedException : $code = 401;
-                    break;
-                case $ex instanceof ControllerNotDefinedException :
-                case $ex instanceof ActionNotDefinedException : $code = 403;
-                    break;
-                case $ex instanceof \HttpException: $code = 404;
-                    break;
-                case $ex instanceof \HttpUrlException : $code = 408; //Si time-out !
-                    break;
-                case $ex instanceof \InvalidArgumentException :
-                case $ex instanceof \RuntimeException : $code = 500;
-                    break;
-                default: $code = 500;
-                    break;
-            }
-
-            $errorController = new ErrorController();
-            $errorController->displayError($code) ;
+        $errorController = new ErrorController();
+        switch (true) {
+            case $ex instanceof \HttpRequestException :
+                $code = 400;
+                break;
+            case $ex instanceof UnauthorizedException :
+                $code = 401;
+                break;
+            case $ex instanceof ControllerNotDefinedException :
+            case $ex instanceof ActionNotDefinedException :
+                $code = 403;
+                break;
+            case $ex instanceof \HttpException:
+                $code = 404;
+                break;
+            case $ex instanceof \HttpUrlException :
+                $code = 408; //Si time-out !
+                break;
+            case $ex instanceof \InvalidArgumentException :
+            case $ex instanceof \RuntimeException :
+                $code = 500;
+                break;
+            default:
+                $code = 500;
+                break;
+        }
     }
 }
