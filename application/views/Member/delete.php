a49e8eb036e6080808a1c6da84b420177f9e8084
sebastienmunoz <sebastien.munoz@live.be>
2018-04-23 14:49:47 +0200

    member Doctrine adaptation et fix interface

---
+++ application/views/Member/delete.php
@@ -0,0 +1,5 @@
+<?php
+include(__DIR__ . '\..\_shared\header.php');
+echo "Member informations deleted !";
+?>
+<br><br><a href="?controller=member&action=index"> <button  type="button" class="btn btn-success">home page</button></a>
