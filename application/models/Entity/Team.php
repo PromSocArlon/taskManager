ea334556b67659b380b6fd8786f1dc2918184345
Alain-D <2YuvvM4Xg2Mz1426>
2018-04-29 13:32:13 +0200

    Map Team with doctrine

--- application/models/Entity/Team.php
+++ application/models/Entity/Team.php
@@ -2,33 +2,51 @@
 
 namespace app\models\Entity;
 
+//use Doctrine\common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tbl_team")
+ */
 class Team extends Entity
 {
-
+    /** @ORM\Column(type="string") **/
     private $name;
-    /**
-     * @var Member
-     */
+
+    /** @ORM\OneToOne(targetEntity=Member::class)  **/
     private $leader;
+    /**
+     * @ORM\ManyToMany(targetEntity=Member::class)
+     * @ORM\JoinTable(name="tbl_member_team")
+     **/
+    private $members;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Task::class)
+     * @ORM\JoinTable(name="tbl_task_team")
+     **/
+    private $tasks;
 
 
     //public function __construct($idMember)
     public function __construct()
     {
+        $this->members=new ArrayCollection();
         //$this->members[0] = new Member();
     }
 
-    public function setName($new_teamName)
+    public function setName($new_teamName):void
     {                              //save in database
         $this->name = $new_teamName;
     }
 
-    public function getName()
+    public function getName():string
     {                                           // from database
         return $this->name;
     }
 
-    public function setLeader($new_teamLeader)
+    public function setLeader($new_teamLeader):void
     {
         $this->leader = $new_teamLeader;
     }
@@ -38,8 +56,17 @@ class Team extends Entity
         return $this->leader;
     }
 
-    public function entityToArray()
+    public function getMembers()
     {
-        return get_object_vars($this);
+        return $this->members;
     }
+    public function  setMembers($members)
+    {
+        $this->members=$members;
+    }
+
+//    public function entityToArray()
+//    {
+//        return get_object_vars($this);
+//    }
 }
