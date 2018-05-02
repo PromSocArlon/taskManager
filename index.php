2df3b3d5aa893d7eea5986e10954190ec0eef243
Alain-D <2YuvvM4Xg2Mz1426>
2018-05-02 22:08:12 +0200

    Moved autoload to /bootstrap.php

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
 session_start();
-require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__.'/bootstrap.php';
 
-//use function app\core\handelerror;
-//\Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
-//require_once 'application/core/Route.php';
+require_once __DIR__ . '/bootstrap.php';
+
+//use function app\core\handleError;
 
 $route = new \app\core\Route();
 //try {
-    $route->routeQuery($entityManager);
+$route->routeQuery($entityManager);
 //} catch (\Exception $ex) {
 //    \app\core\handleError($ex);
 //}
-
