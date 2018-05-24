4d7ef827068fddb5ca7104728fbc82d310753d04
sebastienmunoz <sebastien.munoz@live.be>
2018-05-24 19:35:29 +0200

    fix index

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 session_start();
-require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__.'/application/core/functions.php';
-
-
+require_once __DIR__ . '/bootstrap.php';
 $route = new \app\core\Route();
 try {
-    $route->routeQuery();
+    $route->routeQuery($entityManager);
 } catch (Error | Exception $ex) {
     \app\core\exceptions\ErrorHandler::handleError($ex);
 }
